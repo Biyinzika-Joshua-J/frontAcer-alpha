8ee7443caeaad70c60cfdf31ae5e7ad445a91c84
Joshua Bizinyika <joshuabiyinzika22@gmail.com>
2023-11-08 20:12:09 +0300

    added a heat map

--- src/app/(dashboard)/learn/profile/page.tsx
+++ src/app/(dashboard)/learn/profile/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import HeatMap from "@/components/heatmap/HeatMap";
+
 
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div>
+      <div className="h-56 w-full bg-black relative">
+        <div className="profile-bg__image"></div>
+        <div className="absolute profile-image ">
+          <Image
+            src={
+              "https://cdn.pixabay.com/photo/2016/11/18/19/07/happy-1836445_1280.jpg"
+            }
+            alt="profile image"
+            width={200}
+            height={200}
+            className="h-56 w-56 object-cover rounded-full"
+          />
+        </div>
+      </div>
 
-export default Profile
+      <div className="pt-36">
+        heat map goes here
+       <HeatMap/>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
